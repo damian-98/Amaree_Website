cd7b399f5066953c65d7c3fbc5ec90e1fdad21fd
Damian Tomlinson <secretchaff@gmail.com>
2022-08-04 22:41:42 -0400

    Started Development

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="footer-copyright">
+      <div>
+        Copyright &copy; {new Date().getFullYear()} Beauty Bundles by A'maree
+        Wilford
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+
+function navBar() {
+  return (
+    <div className="shopping-cart">
+      <AiOutlineShoppingCart />
+    </div>
+  );
+}
+
+export default navBar;
