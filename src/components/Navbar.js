08ee924a60a15a16c23274353fd6ea04f6049311
Damian Tomlinson <secretchaff@gmail.com>
2022-08-07 23:55:39 -0400

    Created a navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,32 @@
 import React from "react";
 import { AiOutlineShoppingCart } from "react-icons/ai";
+// import { Link } from "react-router-dom";
 
 function navBar() {
   return (
-    <div className="shopping-cart">
-      <AiOutlineShoppingCart />
-    </div>
+    <nav className="nav">
+      <li>
+        <a href="/home">Home</a>
+      </li>
+      <li>
+        <a href="/menu">Menu</a>
+      </li>
+      <li>
+        <a href="/images">Images</a>
+      </li>
+      <li>
+        <a href="/contact">Contact</a>
+      </li>
+      <li>
+        <a href="/about">About</a>
+      </li>
+      <li>
+        <a href="/checkout" className="shopping-cart">
+          {" "}
+          <AiOutlineShoppingCart />
+        </a>
+      </li>
+    </nav>
   );
 }
 
