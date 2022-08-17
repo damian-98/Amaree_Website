603af3fd12773ee48cb50627132b5ad215ca952e
Damian Tomlinson <secretchaff@gmail.com>
2022-08-16 22:58:51 -0400

    made more change to css and added to about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,52 @@
 import React from "react";
+import Gif from "./img/IMG_0723.gif";
 
 function About() {
   return (
-    <div className="About-Page-Title">
-      <h1>Hello</h1>
+    <div>
+      <div class="about-section">
+        <h1>About</h1>
+        <p>Some text about who we are and what we do.</p>
+        <p>
+          Resize the browser window to see that this page is responsive by the
+          way.
+        </p>
+        <p>
+          Resize the browser window to see that this page is responsive by the
+          way.
+        </p>
+        <p>
+          Resize the browser window to see that this page is responsive by the
+          way.
+        </p>
+        <p>
+          Resize the browser window to see that this page is responsive by the
+          way.
+        </p>{" "}
+        <p>
+          Resize the browser window to see that this page is responsive by the
+          way.
+        </p>
+        <p>
+          Resize the browser window to see that this page is responsive by the
+          way.
+        </p>{" "}
+        <p>
+          Resize the browser window to see that this page is responsive by the
+          way.
+        </p>{" "}
+        <p>
+          Resize the browser window to see that this page is responsive by the
+          way.
+        </p>{" "}
+        <p>
+          Resize the browser window to see that this page is responsive by the
+          way.
+        </p>{" "}
+      </div>
+      <div className="about-picture">
+        <img src={Gif} alt="Posing Pic" width="620" height="770"></img>
+      </div>
     </div>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,8 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="footer-copyright">
-      <div className="footer-background">
+    <footer>
+      <div className="footer-copyright">
         Copyright &copy; {new Date().getFullYear()} Beauty Bundles by A'maree
       </div>
     </footer>
