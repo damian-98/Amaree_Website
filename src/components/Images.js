290e059d1e80f546976e7fc10f491e611edbe375
Damian Tomlinson <secretchaff@gmail.com>
2022-08-18 21:42:18 -0400

    added images to image tab and edit css

--- src/components/Images.js
+++ src/components/Images.js
@@ -1,9 +1,32 @@
 import React from "react";
+import image1 from "./img/IMG_2031.jpeg";
+import image2 from "./img/IMG_2033.jpeg";
+import image3 from "./img/IMG_2034.jpeg";
 
 function Images() {
   return (
-    <div>
-      <h1>Images</h1>
+    <div className="wrapper">
+      <img
+        src={image1}
+        className="container"
+        alt="mirror selfie"
+        width="520"
+        height="320"
+      ></img>
+      <img
+        src={image2}
+        className="container"
+        alt="mirror selfie"
+        width="520"
+        height="320"
+      ></img>
+      <img
+        src={image3}
+        className="container"
+        alt="mirror selfie"
+        width="520"
+        height="320"
+      ></img>
     </div>
   );
 }
